2a8620332ef737ef3f19bca17c2e5c19987e3b09
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-08-30 21:28:06 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -12,19 +12,19 @@ INPUT_SCHEMA = {
         'example': ["Inferless_"]
     },
     "frame_load_cap": {
-        'datatype': 'INT8',
+        'datatype': 'INT64',
         'required': True,
         'shape': [1],
         'example': [60]
     },
     "input_width": {
-        'datatype': 'INT8',
+        'datatype': 'INT64',
         'required': True,
         'shape': [1],
         'example': [360]
     },
     "input_height": {
-        'datatype': 'INT8',
+        'datatype': 'INT64',
         'required': True,
         'shape': [1],
         'example': [640]
@@ -54,37 +54,37 @@ INPUT_SCHEMA = {
         'example': ["purple lilacs, vibrant, dense, natural, fresh, close-up, detailed petals, bright colors"]
     },
     "output_width": {
-        'datatype': 'INT8',
+        'datatype': 'INT64',
         'required': True,
         'shape': [1],
         'example': [720]
     },
     "output_height": {
-        'datatype': 'INT8',
+        'datatype': 'INT64',
         'required': True,
         'shape': [1],
         'example': [1280]
     },
     "hires_steps": {
-        'datatype': 'INT8',
+        'datatype': 'INT64',
         'required': True,
         'shape': [1],
         'example': [5]
     },
     "hires_upscale": {
-        'datatype': 'FP32',
+        'datatype': 'FP64',
         'required': True,
         'shape': [1],
         'example': [1.2]
     },
     "hires_denoise": {
-        'datatype': 'FP32',
+        'datatype': 'FP64',
         'required': True,
         'shape': [1],
         'example': [0.56]
     },
     "seed": {
-        'datatype': 'INT8',
+        'datatype': 'INT64',
         'required': True,
         'shape': [1],
         'example': [127]
