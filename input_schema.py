92051cf7e698dc6983ec6f6e55d5c746fa7094b7
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-08-30 19:26:15 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -1,20 +1,86 @@
 INPUT_SCHEMA = {
-    "workflow": {
-        'datatype': 'BYTES',
+    "input_video_url": {
+        'datatype': 'STRING',
         'required': True,
         'shape': [1],
-        'example': ["txt_2_img"]
+        'example': ["https://sukuru.s3.amazonaws.com/videos/db76940a-db5f-4578-8f87-9db41af32647.MP4"]
     },
-    "positive_token": {
-        'datatype': 'BYTES',
+    "frame_load_cap": {
+        'datatype': 'INT8',
         'required': True,
         'shape': [1],
-        'example': ["There is a fine house in the forest"]
+        'example': [60]
     },
-    "negative_token": {
-        'datatype': 'BYTES',
+    "input_width": {
+        'datatype': 'INT8',
         'required': True,
         'shape': [1],
-        'example': ["There is a fine house in the forest"]
+        'example': [360]
+    },
+    "input_height": {
+        'datatype': 'INT8',
+        'required': True,
+        'shape': [1],
+        'example': [640]
+    },
+    "prompt_styler": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["(ultra high res:1.4), (masterpiece), (beautiful lighting:1.4) , Bright sunlight illuminates , (mesmerizing:1.5), colorful, vibrant,"]
+    },
+    "ipadapter_image_url": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["https://sukuru.s3.amazonaws.com/custom_themes_main/Flowers/purple-lilac.jpg"]
+    },
+    "checkpoint": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["dreamshaper_8.safetensors"]
+    },
+    "input_prompt": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["purple lilacs, vibrant, dense, natural, fresh, close-up, detailed petals, bright colors"]
+    },
+    "output_width": {
+        'datatype': 'INT8',
+        'required': True,
+        'shape': [1],
+        'example': [720]
+    },
+    "output_height": {
+        'datatype': 'INT8',
+        'required': True,
+        'shape': [1],
+        'example': [1280]
+    },
+    "hires_steps": {
+        'datatype': 'INT8',
+        'required': True,
+        'shape': [1],
+        'example': [5]
+    },
+    "hires_upscale": {
+        'datatype': 'FP16',
+        'required': True,
+        'shape': [1],
+        'example': [1.2]
+    },
+    "hires_denoise": {
+        'datatype': 'FP16',
+        'required': True,
+        'shape': [1],
+        'example': [0.56]
+    },
+    "seed": {
+        'datatype': 'INT8',
+        'required': True,
+        'shape': [1],
+        'example': [127]
     }
 }
