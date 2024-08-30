055d659290523864b5cf67a6baf667e4762c1158
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-08-30 15:13:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+import os
+import subprocess
+import logging
+import requests
 
+class InferlessPythonModel:
+    def initialize(self):
+        subprocess.run(["wget", "https://github.com/rbgo404/Files/raw/main/build.sh"])
+        subprocess.run(["bash", "build.sh"], check=True)
+
+    def infer(self, inputs):
+        return {"status":"GOOD"}
+
+    def finalize(self):
+      pass
